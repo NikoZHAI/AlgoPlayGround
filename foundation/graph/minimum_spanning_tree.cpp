2dcca39f32d9450b9e9dbb4ef4ce73c570193bb8
NikoZHAI <huanyu.zhai@etu.unice.fr>
2020-02-02 11:46:22 +0800

    done MST

--- foundation/graph/minimum_spanning_tree.cpp
+++ foundation/graph/minimum_spanning_tree.cpp
@@ -1,33 +1,62 @@
 #include "minimum_spanning_tree.hpp"
-#include "undirected_graph.hpp"
-#include "undirected_graph_vertex.hpp"
 
 
 #include <vector>
 #include <utility>
 #include <queue>
 #include <set>
+#include <algorithm>
+#include <string>
 
 
 using namespace algo;
 
 
-std::pair<long long int, std::vector<int> >
-primMST( UVertex *root )
+std::pair<long long int, std::vector<unsigned int> >
+primMST( const wAdjList &adjList, unsigned int root )
 {
-    long long int sum = 0;
-    std::vector<int> order;
-    if ( !root )
-        return std::make_pair( sum, order );
+    long long int costSum = 0;
+    std::size_t vertexCount = adjList.size();
+    std::vector<unsigned int> perm; // permutation of the resulting MST
 
-    std::priority_queue< std::pair<long long int, UVertex*> > Q;
-    std::set<int> visited;
+    if ( !vertexCount ) return std::make_pair( costSum, perm );
 
-    Q.emplace( 0, root );
-    while ( !Q.empty() )
+    std::priority_queue<wPair, std::vector<wPair>, std::less<wPair> > Q;
+    std::vector<bool> visited( vertexCount, false );
+
+    // enqueue the root of MST
+    if ( root < vertexCount )
     {
-        
+        Q.push( std::make_pair( 0ll, root ) );
     }
 
-    return std::make_pair( sum, order );
+    perm.reserve( vertexCount );
+    while ( !Q.empty() )
+    {
+        wPair pivot = Q.top();
+        Q.pop();
+        
+        if ( visited[pivot.second] )
+        {
+            continue;
+        }
+
+        costSum += pivot.first;
+        perm.push_back( pivot.second );
+        for( const wPair &adj : adjList[pivot.second] )
+        {
+            if ( !visited[adj.second] )
+                Q.push(adj);
+        }
+        visited[pivot.second] = true;
+    }
+
+    // check if all vertices have been visited
+    for ( std::size_t i = 0; i < vertexCount; ++i )
+    {
+        if ( !visited[i] )
+            throw "Vertex(" + std::to_string(i) + ") is disconnected";
+    }
+
+    return std::make_pair( costSum, perm );
 }
--- foundation/graph/minimum_spanning_tree.hpp
+++ foundation/graph/minimum_spanning_tree.hpp
@@ -9,18 +9,27 @@
 
 namespace algo {
 
-
-class UVertex;
+/**
+ * @brief Representation of a weighted adjacency list (aka. WAL)
+ * 
+ * Each row of the WAL represents a vertex as well as its adjacent vertices in the graph.
+ * Each element in the WAL is presented as a pair of weight and
+ * the index of this very adjacent vertex in the WAL. With this representation, we duplicate
+ * exactly one time the adjacency infomation. ex. If row (0) has element <2, 5>, row (5) must
+ * have <2, 0>.
+*/
+using wPair = std::pair<long long int, unsigned int>;
+using wAdjList = std::vector< std::vector< wPair > >;
 
 
 /**
  * @brief Finding the minimum spanning tree of an undirected-graph using Prim's Algorithm
  * 
- * @param graph the undirected-graph whose MST is desired
- * @return std::vector<int> the unique id of the vertices in the MST 
+ * @param adjList the adjacency list representation of the undirected-graph whose MST is desired
+ * @return std::pair<cost of the MST, the permutation of the vertices in the MST>
  */
-std::pair< long long int, std::vector<int> >
-primMST( UVertex *root );
+std::pair<long long int, std::vector<unsigned int> >
+primMST( const wAdjList &adjList, unsigned int root = 0 );
 
 
 }   // namespace algo
